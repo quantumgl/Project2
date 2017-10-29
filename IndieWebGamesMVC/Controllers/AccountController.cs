f2d90657c5b172a936e0d18d7515310127c17812
Andres Gonzalez <andresgl@live.com>
2017-10-29 00:55:01 -0400

    External logins with Usernames. Adding to master by accident!

--- IndieWebGamesMVC/Controllers/AccountController.cs
+++ IndieWebGamesMVC/Controllers/AccountController.cs
@@ -75,7 +75,7 @@ namespace IndieWebGamesMVC.Controllers
 
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            var result = await SignInManager.PasswordSignInAsync(model.Username, model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
             {
                 case SignInStatus.Success:
@@ -151,7 +151,7 @@ namespace IndieWebGamesMVC.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Name, Email = model.Email };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
@@ -367,7 +367,7 @@ namespace IndieWebGamesMVC.Controllers
                 {
                     return View("ExternalLoginFailure");
                 }
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Username, Email = model.Email }; //TODO: Get email from external service
                 var result = await UserManager.CreateAsync(user);
                 if (result.Succeeded)
                 {
