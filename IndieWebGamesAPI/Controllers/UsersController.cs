bdc5d631edf45bd596d32e7766d69d37c63b1f5d
Esteban Diaz <dexterprivate@msn.com>
2017-10-29 21:15:08 -0400

    test de api01

--- IndieWebGamesAPI/Controllers/UsersController.cs
+++ IndieWebGamesAPI/Controllers/UsersController.cs
@@ -12,18 +12,27 @@ namespace IndieWebGamesAPI.Controllers
 {
     public class UsersController : ApiController
     {
-        private static List<UserStatus> loggedinusers;
+       
+        //private static List<UserStatus> loggedinusers;
+        private static List<UserStatus> loggedinusers = new List<UserStatus>{
+         new UserStatus {  Name = "Mark", LastPing =
+            DateTime.Now},
+         new UserStatus {  Name = "Joe", LastPing =
+            DateTime.Now },
+      };
+
 
         //private Predicate<UserStatus> comparerer = new Predicate<UserStatus> { userstat => userstat}
         private applicationDbContext context;
         // api/Users
 
-        public UserStatus GetCurrentUsers()
+        public IList<UserStatus> GetCurrentUsers()
         {
             
             DateTime updated = DateTime.Now.Add(new TimeSpan(0, 0, -2));
-         
-            var users = loggedinusers.Find(current => (current.LastPing <= DateTime.Now) && (current.LastPing > updated));
+        
+            var users = loggedinusers.FindAll(current => ((current.LastPing <= DateTime.Now) && (current.LastPing > updated)));
+            
 
             return (users);
         }
