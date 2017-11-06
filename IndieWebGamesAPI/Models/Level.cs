ce70665251fa5a0a2540c72baa756b9c18ab892e
Andres Gonzalez <andresgl@live.com>
2017-11-06 02:25:54 -0400

    foreign key attempt

--- IndieWebGamesAPI/Models/Level.cs
+++ IndieWebGamesAPI/Models/Level.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,8 @@ namespace IndieWebGamesAPI.Models
     public class Level
     {
         public int Id { get; set; }
-        public virtual IndiePlayerProfile Creator { get; set; }
+        [ForeignKey("IndiePlayerProfile")]
+        public int CreatorId { get; set; }
         public string Bgmurl { get; set; }
         public string Bgimgurl { get; set; }
         public string LevelName { get; set; }
