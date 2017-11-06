320782792e91b7c43ed570735a54cdc1a3d5cda1
Esteban Diaz <dexterprivate@msn.com>
2017-11-05 19:56:08 -0500

    classes Levels and update IndiePlayerProfile

---
+++ IndieWebGamesAPI/Models/Levels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IndieWebGamesAPI.Models
+{
+    public class Levels
+    {
+        public int Id { get; set; }
+        public IndiePlayerProfile Creator { get; set; }
+        public string Bgmurl { get; set; }
+        public string Bgimgurl { get; set; }
+        public string LevelName { get; set; }
+    }
+}
